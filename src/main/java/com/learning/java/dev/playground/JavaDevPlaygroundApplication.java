0013c64268edd76c8359846e92d950c5c251a698
Rushikesh Sonawane <r.sonawane@serrala.com>
2025-05-30 22:37:29 +0530

    Initial commit

---
+++ src/main/java/com/learning/java/dev/playground/JavaDevPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.learning.java.dev.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/java/dev/playground/JavaDevPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.java.dev.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
